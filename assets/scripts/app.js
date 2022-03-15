2c15b7227dcb4974e2e957bfbecd2393bac4484c
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-03-14 23:26:28 -0400

    init commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,36 @@
+const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
+
+let chosenMaxLife = 100;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+
+adjustHealthBars(chosenMaxLife);
+
+function attackHandler() {
+  const damage = dealMonsterDamage(ATTACK_VALUE);
+  if (damage === 0) {
+    alert('You missed??');
+  } else if (damage === 1) {
+    alert('You dropped ' + damage + ' point of damage on that beesh!');
+  } else {
+    alert('You dropped ' + damage + ' points of damage on that beesh!');
+  }
+  currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  if (playerDamage === 0) {
+    alert('They missed!!');
+  } else if (playerDamage === 1) {
+    alert('Ramsey sliced you for ' + damage + ' point of damage!');
+  } else {
+    alert('Ramsey sliced you for ' + damage + ' points of damage!');
+  }
+  currentPlayerHealth -= playerDamage;
+  if (currentMonsterHealth <= 0) {
+    alert('You Won');
+  } else if (currentPlayerHealth <= 0) {
+    alert('You lost!');
+  }
+}
+
+attackBtn.addEventListener('click', attackHandler);
