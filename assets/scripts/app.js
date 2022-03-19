932e0a23708219b6b250c7d7eaa8ecdd1ab17bb3
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-03-19 19:21:16 -0400

    cleaned up code a little bit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,14 +3,105 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 11;
 const PLAYER_HEAL_VALUE = 18;
 
-let chosenMaxLife = 100;
+const ATTACK_MODE = 'ATTACK'
+const STRONG_ATTACK_MODE = 'STRONG_ATTACK'
+const LOG_EVENT_PLAYER_ATTACK = 'PLAYER_ATTACK'
+const LOG_EVENT_PLAYER_STRONG_ATTACK = 'PLAYER_STRONG_ATTACK'
+const LOG_EVENT_MONSTER_ATTACK = 'MONSTER_ATTACK'
+const LOG_EVENT_PLAYER_HEAL = 'PLAYER_HEAL'
+const LOG_EVENT_GAME_OVER = 'GAME_OVER'
+
+
+const enteredValue = prompt('Maximum life for you and the monster.', '100')
+
+let chosenMaxLife = parseInt(enteredValue);
+
+let battleLog =[]
+
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+  chosenMaxLife = 100
+}
+
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife =true
 
 adjustHealthBars(chosenMaxLife);
 
+function writeToLog(event, value, monsterHealth, playerHealth) {
+ let logEntry;
+ if (event === LOG_EVENT_PLAYER_ATTACK) {
+   logEntry = {
+     event: event,
+     value: value,
+     target: "MONSTER",
+     finalMonsterHealth: monsterHealth,
+     finalPlayerHealth: playerHealth
+   }
+   
+ } else if (event === LOG_EVENT_PLAYER_STRONG_ATTACK) {
+  logEntry = {
+    event: event,
+    value: value,
+    target: "MONSTER",
+    finalMonsterHealth: monsterHealth,
+    finalPlayerHealth: playerHealth
+  }
+  
+} else if (event === LOG_EVENT_MONSTER_ATTACK) {
+  logEntry = {
+    event: event,
+    value: value,
+    target: "PLAYER",
+    finalMonsterHealth: monsterHealth,
+    finalPlayerHealth: playerHealth
+  }
+  
+} else if (event === LOG_EVENT_PLAYER_HEAL) {
+  logEntry = {
+    event: event,
+    value: value,
+    target: "PLAYER",
+    finalMonsterHealth: monsterHealth,
+    finalPlayerHealth: playerHealth
+  }
+  
+} else if (event === LOG_EVENT_GAME_OVER) {
+  logEntry = {
+    event: event,
+    value: value,
+    
+    finalMonsterHealth: monsterHealth,
+    finalPlayerHealth: playerHealth
+  }
+  
+}
+battleLog.push(logEntry)
+}
+
+
+function reset() {
+  currentMonsterHealth = chosenMaxLife;
+ currentPlayerHealth = chosenMaxLife;
+ resetGame()
+}
+
 function endRound() {
+  const initalPlayerHealth = currentPlayerHealth
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+  writeToLog(LOG_EVENT_MONSTER_ATTACK, 
+    playerDamage, 
+    currentMonsterHealth, 
+    currentPlayerHealth)
+
+  if (currentPlayerHealth <= 0 && hasBonusLife) {
+    hasBonusLife = false; 
+    removeBonusLife();
+      currentPlayerHealth = initalPlayerHealth;
+      setPlayerHealth(initalPlayerHealth)
+      ('You have invoked Soul Restore')
+  }
   if (playerDamage === 0) {
     alert('They missed!!');
   } else if (playerDamage === 1) {
@@ -18,23 +109,49 @@ function endRound() {
   } else {
     alert('Ramsey sliced you for ' + playerDamage + ' points of damage!');
   }
-  currentPlayerHealth -= playerDamage;
+  
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert('You Won');
+    writeToLog(LOG_EVENT_GAME_OVER, 
+      'BROWNCOAT WON', 
+      currentMonsterHealth, 
+      currentPlayerHealth)
+  
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
     alert('You lost!');
+
+    writeToLog(LOG_EVENT_GAME_OVER, 
+      'THE ALLIANCE WON', 
+      currentMonsterHealth, 
+      currentPlayerHealth)
+    reset()
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
     alert("You're both dead!");
+    writeToLog(LOG_EVENT_GAME_OVER, 
+      'WE"RE ALL DEAD', 
+      currentMonsterHealth, 
+      currentPlayerHealth)
+    reset()
   }
 }
 
-function attackMonster(attackStrength) {
-  let maxDamage;
-  if (mode === 'ATTACK') {
-    maxDamage = ATTACK_VALUE;
-  } else if (mode == 'STRONG_ATTACK') {
-    maxDamage = STRONG_ATTACK_VALUE;
-  }
+function attackMonster(mode) {
+  const maxDamage = mode === ATTACK_MODE
+   ? ATTACK_VALUE
+    : STRONG_ATTACK_VALUE
+  const logEvent = mode === ATTACK_MODE
+   ? LOG_EVENT_PLAYER_ATTACK 
+   : LOG_EVENT_PLAYER_STRONG_ATTACK
+
+
+  // below code is the same as above. above I refactored with ternary operator
+  // if (mode === ATTACK_MODE) {
+  //   maxDamage = ATTACK_VALUE;
+  //   logEvent = LOG_EVENT_PLAYER_ATTACK;
+  // } else if (mode == STRONG_ATTACK_MODE) {
+  //   maxDamage = STRONG_ATTACK_VALUE;
+  //   logEvent = LOG_EVENT_PLAYER_STRONG_ATTACK
+  // }
   const damage = dealMonsterDamage(maxDamage);
   if (damage === 0) {
     alert('You missed??');
@@ -44,11 +161,15 @@ function attackMonster(attackStrength) {
     alert('You dropped ' + damage + ' points of damage on that beesh!');
   }
   currentMonsterHealth -= damage;
-  endRound();
+  writeToLog(logEvent, 
+    damage, 
+    currentMonsterHealth, 
+    currentPlayerHealth)
+  // endRound();
 }
 
 function attackHandler() {
-  attackMonster('ATTACK');
+  attackMonster(ATTACK_MODE);
   // moved into the other function to just use maxdamge to change function based on attack params
   // const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
   // if (damage === 0) {
@@ -75,17 +196,36 @@ function attackHandler() {
   // } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
   //   alert("You're both dead!");
   // }
+  endRound()
 }
 
 function strongAttackHandler() {
-  attackMonster('STRONG_ATTACK');
+  attackMonster(STRONG_ATTACK_MODE);
+  endRound()
 }
 
 function lazarusHandler() {
+let healValue;
+if (currentPlayerHealth >= chosenMaxLife - PLAYER_HEAL_VALUE) {
+  alert("You can't heal past your max health");
+  healValue = chosenMaxLife - currentPlayerHealth
+} else {
+  healValue = PLAYER_HEAL_VALUE
+}
   increasePlayerHealth(PLAYER_HEAL_VALUE);
+  currentPlayerHealth(PLAYER_HEAL_VALUE)
+  writeToLog(LOG_EVENT_PLAYER_HEAL, 
+    healValue, 
+    currentMonsterHealth, 
+    currentPlayerHealth)
   endRound();
+}
+
+function printLogHandler() {
+  console.log(battleLog)
 }
 
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
 healBtn.addEventListener('click', lazarusHandler);
+logBtn.addEventListener('click', printLogHandler)
